35a8cb2d0ac43b8bc3747a8910319603dd9d9d67
Stephen Moore <stephenmoore33@outlook.com>
2021-04-16 17:40:59 -0400

    added array method for lowercase/uppercase characters

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -5,25 +5,38 @@ var generateBtn = document.querySelector("#generate");
 
 //array of options
 var numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
-var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCase = String.prototype.toUpperCase.apply(letters).split(",");
-var lowerCase = String.prototype.toLowerCase.apply(letters).split(",");
-console.log(upperCase);
-console.log(lowerCase);
+var letters = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ];
+// var upperCase = String.prototype.toUpperCase.apply(letters).split(",");
+// var lowerCase = String.prototype.toLowerCase.apply(letters).split(",");
+
+var upperCase = letters.map(letters => letters.toUpperCase());
+var lowerCase = letters.map(letters => letters.toLowerCase());
+
+//code here
 
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  //call function to prompt users and generate password
 
-  passwordText.value = password;
 
-}
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    //call function to prompt users and generate password
+
+    passwordText.value = password;
+
+  }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 //make a function to generate a password
+
+
+/*
+
+
+
+*/
